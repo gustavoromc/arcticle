a8fda6f27cc13a749fd9cc323d77f452a7d8b3e4
Victor Gustavo Romero Cisneros <gustavoromguest@gmail.com>
2023-07-01 22:20:12 -0600

    Initial structure

---
+++ src/test/java/com/gustavoromc/arcticle/ArcticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoromc.arcticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
